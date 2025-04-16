cc23e7216f4a3a5892828f416366dd9e4428ee48
Akira-zyh <3429629040@qq.com>
2025-04-17 00:43:07 +0800

    update image preprocess

--- controllers/init/image_process.py
+++ controllers/init/image_process.py
@@ -7,11 +7,15 @@ img = cv2.imread(folder_path + 'easy1_104.png')
 height, width = img.shape[:2]
 mask = np.zeros((height, width), dtype=np.uint8)
 
+half_height = height // 2
+mask[half_height:, :] = 255
+
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 lower_green = np.array([35, 128, 46])  # 提高饱和度下限减少干扰
 upper_green = np.array([77, 255, 255])
-mask = cv2.inRange(hsv, lower_green, upper_green)
-blurred = cv2.GaussianBlur(mask, (5, 5), 0)
+color_mask = cv2.inRange(hsv, lower_green, upper_green)
+combined_mask = cv2.bitwise_and(color_mask, mask)
+blurred = cv2.GaussianBlur(combined_mask, (5, 5), 0)
 edges = cv2.Canny(blurred, 50, 150)
 contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 blank_image = np.zeros_like(img)
