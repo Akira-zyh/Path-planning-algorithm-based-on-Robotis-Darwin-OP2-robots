91dab469137196fede617caa5f84792f2ba774be
Akira-zyh <3429629040@qq.com>
2025-04-22 23:20:25 +0800

    update map

--- controllers/test/test.py
+++ controllers/test/test.py
@@ -1,12 +1,11 @@
 """test controller."""
-
+import sys
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
-from controller import Supervisor
-from managers import RobotisOp2GaitManager,RobotisOp2MotionManager
-import sys
+from controller import Robot
+from managers import RobotisOp2GaitManager
 # create the Robot instance.
-robot = Supervisor()
+robot = Robot()
 
 NMOTORS = 20
 motorNames = [
@@ -16,29 +15,8 @@ motorNames = [
     "FootL", "Neck", "Head"
 ]
 
-# get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
-# lidar = robot.getDevice("lidar")
-# lidar.enable(timestep)
-# lidar.enablePointCloud()
-# robot.simulationResetPhysics()
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getDevice('motorname')
-#  ds = robot.getDevice('dsname')
-#  ds.enable(timestep)
-robot.step(timestep)
-gait_manager = RobotisOp2GaitManager(robot, "config.ini")
-gait_manager.start()
-motion_manager = RobotisOp2MotionManager(robot)
-gyro = robot.getDevice("Gyro")
-gyro.enable(timestep)
-led_head = robot.getDevice("HeadLed")
-led_eye = robot.getDevice("EyeLed")
-led_head.set(0xFFFF00)
-led_eye.set(0xFF0400)
 motors = []
-print(timestep)
 position_sensors = []
 for name in motorNames:
     motor = robot.getDevice(name)
@@ -46,46 +24,39 @@ for name in motorNames:
     sensor.enable(timestep)
     motors.append(motor)
     position_sensors.append(sensor)
-# 传感器初始化（网页4示例）
-robot.accelerometer = robot.getDevice("Accelerometer")
-robot.accelerometer.enable(timestep)
-
-def wait(ms):
-    start_time = robot.getTime()
-    while (robot.getTime() - start_time) * 1000 < ms:
-        my_step()
-def my_step():
-    if robot.step(timestep) == -1:
+# get the time step of the current world.
+accelerometer = robot.getDevice("Accelerometer")
+accelerometer.enable(timestep)
+gyro = robot.getDevice("Gyro")
+gyro.enable(timestep)
+def my_step(self):
+    if robot.step(self.timeStep) == -1:
         sys.exit(0)
-        
-my_step()
-# 初始化动作
-motion_manager.playPage(9)  # 初始姿势
-motors[-1].setPosition(0.8)
-wait(200)
-my_step()
-
-    
+# You should insert a getDevice-like function in order to get the
+# instance of a device of the robot. Something like:
+#  motor = robot.getDevice('motorname')
+#  ds = robot.getDevice('dsname')
+#  ds.enable(timestep)
+gait_manager = RobotisOp2GaitManager(robot, "config.ini")
+gait_manager.setBalanceEnable(True)
+gait_manager.start()
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
-while True:
+while robot.step(timestep) != -1:
     # Read the sensors:
     # Enter here functions to read sensor data, like:
     #  val = ds.getValue()
-    # data = lidar.getPointCloud()
+    gait_manager.setXAmplitude(1.0)
+    gait_manager.setAAmplitude(-0.5)
+    i = 0
+    if i >= 10:
+        gait_manager.setXAmplitude(1.0)
+        gait_manager.setAAmplitude(-0.5)
+        i += 1
     # Process sensor data here.
-    # gait_manager.setXAmplitude(1.0)
-    gait_manager.setYAmplitude(0.5)
-    gait_manager.setAAmplitude(0.0)
-    
-    
-    # random_action = np.random.randint(0, 4)
-    # robot.execute_action(random_action)
-    # print(random_action)
-    
-    # 步态更新
     gait_manager.step(timestep)
-    my_step()
     # Enter here functions to send actuator commands, like:
+    #  motor.setPosition(10.0)
+    pass
 
 # Enter here exit cleanup code.
